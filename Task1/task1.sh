af4304301f4ce4b6ebfcba7008e0cf2ea5c2890c
Muhammad Asad <m.asadd07@gmail.com>
2023-10-31 15:52:58 +0500

    Revert "Modified variable names"
    This reverts commit d75be625765a3807a6f83472a11920fef062b5f3.

--- Task1/task1.sh
+++ Task1/task1.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 function search_file {
-    temp_file=$1
-    temp_folder=$2
+    temp_fi=$1
+    temp_fo=$2
 
-    for f in $temp_folder/*; do
+    for f in $temp_fo/*; do
         if [ -d $f ]; then
-            search_file $temp_file $f
+            search_file $temp_fi $f
         fi
         file_name=$(basename $f)
-        if [ $file_name == $temp_file ]; then
+        if [ $file_name == $temp_fi ]; then
             echo "File exists"
-            echo "$temp_folder/$temp_file"
+            echo "$temp_fo/$temp_fi"
             exit 1
         fi
     done
