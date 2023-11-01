8761babecea5ecd3109364ce82f6c88fd2de1baf
Muhammad Asad <m.asadd07@gmail.com>
2023-11-01 18:01:09 +0500

    Makind directories in Task 2, Manage space case string in Task5, Adding case statement in Task6

--- Task5/box_print.sh
+++ Task5/box_print.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-length_of_string=${#1}
-length_of_string=$(($length_of_string + 4))
-temp=0
-if [[ ("$1" == "")]]; then
+if [[ ("$1" == "") ]]; then
     echo "Invalid number of arguments"
-    exit 1 
+    exit 1
 fi
+length_of_string=0
+length_of_spaces=0
+for i in $@; do
+    length_of_spaces=$(($length_of_spaces + 1))
+    length_of_string=$(($length_of_string + ${#i}))
+done
+length_of_spaces=$(($length_of_spaces - 1))
+length_of_string=$(($length_of_string + $length_of_spaces))
+length_of_string=$(($length_of_string + 2))
+echo $length_of_string
+temp=0
 while [ $length_of_string -gt $temp ]; do
     echo -n "*"
     temp=$(($temp + 1))
 done
-echo -e "\n*" $1 "*"
+echo -e -n "\n*"
+for i in $@; do
+    echo -n $i
+    if [ $length_of_spaces -gt 0 ]; then
+         echo -n " "
+         length_of_spaces=$(($length_of_spaces - 1))
+    fi
+done
+echo "*"
 temp1=0
 while [ $length_of_string -gt $temp1 ]; do
     echo -n "*"
--- Task6/box_print.sh
+++ Task6/box_print.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-#initializing length of input string
-Length_of_string=${#2}
-Length_of_string=$(($Length_of_string + 4))
-#check if there is nothing to print
-if [[ ("$1" == "")]]; then
-    echo "Invalid number of arguments"
-    exit 1 
-fi
-#check if 2nd argument is not given just print the string
-if [[ ("$4" == "")]]; then
-    echo $2
-    exit 1 
-fi
-temp=0
-while [ $Length_of_string -gt $temp ]; do
-    echo -n $4
-    temp=$(($temp + 1))
+function print_box() {
+    Length_of_string=${#2}
+    Length_of_string=$(($Length_of_string + 4))
+    if [[ ("$1" == "") ]]; then
+        echo $2
+        exit 1
+    fi
+    if [[ ("$2" == "") ]]; then
+        echo $2
+        exit 1
+    fi
+    temp=0
+    while [ $Length_of_string -gt $temp ]; do
+        echo -n $1
+        temp=$(($temp + 1))
+    done
+    echo -e "\n"$1 $2 $1
+    temp1=0
+    while [ $Length_of_string -gt $temp1 ]; do
+        echo -n $1
+        temp1=$(($temp1 + 1))
+    done
+    echo -e "\n"
+}
+str=""
+char=""
+while getopts 'c:s:' flag; do
+    case "${flag}" in
+    c) char="${OPTARG}";;
+    s) str="${OPTARG}";;
+    *) echo "Illegal arguments"       
+        exit 1;;
+    esac
 done
-echo -e "\n"$4 $2 $4
-temp1=0
-while [ $Length_of_string -gt $temp1 ]; do
-    echo -n $4
-    temp1=$(($temp1 + 1))
-done
-echo -e "\n"
+print_box $char "$str"
